8b3a43a245368908905c035fe9fbbb58e881d0a4
Leandro Ugioni <leandro_ugioni@hotmail.com>
2024-11-13 13:08:23 -0300

    [wip] actions improvement

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,8 +1,13 @@
 import Example from '../requests/Example.request.js';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
 import { group } from 'k6';
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
 
-const example = new Example();
+//const example = new Example();
+
+export const getContactsDuration = new Trend('get_contacts', true);
 
 export const options = {
   thresholds: {
@@ -24,8 +29,34 @@ export function handleSummary(data) {
 
 //export default function (token) {
 export default function () {
-  group('post pdf', function () {
+  const baseUrl = 'https://test.k6.io/';
+  /* group('post pdf', function () {
     //example.getContacts(token, binFile);
     example.getContacts();
+  });*/
+  const payload = {
+    client_id: '44984984649',
+    grant_type: 'password',
+    username: ' jmfilho@sp.gov.br',
+    password: '123456'
+  };
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const OK = 200;
+
+  //const res = http.get(`${this.baseUrl}`, payload, params);
+  const res = http.get(`${baseUrl}`, params);
+  //const token = 'Bearer ' + res.json('access_token');
+
+  getContactsDuration.add(res.timings.duration);
+
+  check(res, {
+    'get contacts - status 200': () => res.status === OK
   });
+  //return token;
 }
