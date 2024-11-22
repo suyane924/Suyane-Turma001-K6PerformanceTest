a7c2c25bd03a0c14264497bcc9bddb47812652ec
Leandro Ugioni <leandro_ugioni@hotmail.com>
2024-11-22 00:17:00 -0300

    [feat] add web dashboard

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -2,16 +2,21 @@ import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporte
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
 import { check } from 'k6';
-import { Trend } from 'k6/metrics';
+import { Trend, Rate } from 'k6/metrics';
 
 export const getContactsDuration = new Trend('get_contacts', true);
+export const RateContentOK = new Rate('content_OK');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.01'],
-    http_req_duration: ['avg<10000']
+    http_req_failed: ['rate<0.30'],
+    get_contacts: ['p(99)<500'],
+    content_OK: ['rate>0.95']
   },
-  stages: [{ duration: '20s', target: 20 }]
+  stages: [
+    { duration: '10s', target: 5 },
+    { duration: '20s', target: 15 }
+  ]
 };
 
 export function handleSummary(data) {
@@ -35,6 +40,8 @@ export default function () {
   const res = http.get(`${baseUrl}`, params);
 
   getContactsDuration.add(res.timings.duration);
+
+  RateContentOK.add(res.status === OK);
 
   check(res, {
     'GET Contacts - Status 200': () => res.status === OK
