5075de17aa5acfd0ae08f73dfd7e999187878ceb
suyane924 <suyanemk@outlook.com>
2025-06-17 19:45:00 -0300

    k6

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,49 +1,32 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
-
 export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '1m', target: 10 },   
+    { duration: '2m', target: 300 }, 
+    { duration: '2m', target: 0 },    
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<5700'],  
+    'status_rate': ['rate>0.88'],        
+  },
 };
 
-export function handleSummary(data) {
-  return {
-    './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
-  };
-}
+
+const durationTrend = new Trend('duration_trend');
+const statusRate = new Rate('status_rate');
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const res = http.get('https://dummyjson.com/products');
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
+  durationTrend.add(res.timings.duration);
+  statusRate.add(res.status === 200);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'status is 200': (r) => r.status === 200,
   });
+
+  sleep(1);
 }
